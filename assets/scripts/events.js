4997260e7bc357ccd253b43ec96d6deb630c0265
asangurima <asangurima@gmail.com>
2019-02-15 18:04:44 -0500

    Finish html gameboard
    -add padding to cells to center content in theme.css
    -add if statement in events.js to prevent overwriting in cells

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,18 +1,17 @@
 
 let currentPlayer = 'X'
+// my javascript game board for gamelogic:
+// let jsBoard = ['', '', '', '', '', '', '', '', '']
 
 const onMakeMove = (event) => {
   event.preventDefault()
   //  if I grab the data index-number now, I can then put an X in that specific,
   // data index # cell?
   const cellIndexValue = $(event.target).data('index')
-  console.log('I got clicked!')
   console.log('cellIndexValue:', cellIndexValue)
 
-  // want to make an x or o appear in cell
+  // make an x or o appear in cell when clicked; switch players:
   const changePlayer = () => {
-    console.log('changePlayer is running')
-
     if (currentPlayer === 'X') {
       $(event.target).html('X')
       currentPlayer = 'O'
@@ -20,11 +19,11 @@ const onMakeMove = (event) => {
       $(event.target).html('O')
       currentPlayer = 'X'
     }
-    console.log('currentPlayer is:', currentPlayer)
   }
-  changePlayer()
-  // when clicked, if the cell is blank, add a mark (x), if there is a mark already,
-  // then leave the mark there.
+
+  if ($(event.target).text() === '') {
+    changePlayer()
+  }
   // when clicked, if the cell is blank, add a mark (x), if there is a mark already,
   // then leave the mark there.
 
@@ -33,5 +32,5 @@ const onMakeMove = (event) => {
 }
 
 module.exports = {
-  onMakeMove
+  onMakeMove,
 }
