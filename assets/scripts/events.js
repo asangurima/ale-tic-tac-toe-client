165ce5b464b5b1fb6d536020edfca32f498bf6b8
asangurima <asangurima@gmail.com>
2019-02-15 11:54:45 -0500

    Add functions to put X/O and change players
    Currently not switching players but a good start

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -7,10 +7,35 @@ const onMakeMove = (event) => {
   console.log('I got clicked!')
   console.log('cellIndexValue:', cellIndexValue)
   // want to make an x or o appear in cell
+  // $(event.target).html('X')
+
+  const changePlayer = () => {
+    // once leaveYourMark has run, then I should switch from player X to player O
+    console.log('changePlayer is running')
+    let currentPlayer = 'X'
+
+    if (currentPlayer === 'X') {
+      currentPlayer = 'X'
+    } else {
+      currentPlayer = 'O'
+    }
+    console.log('currentPlayer is:', currentPlayer)
+    leaveYourMark(currentPlayer)
+  }
+
+  const leaveYourMark = (player) => {
+    console.log('leaveYourMark is running')
+    console.log('player is:', player)
+    if (player === 'X') {
+      $(event.target).html('X')
+    } else if (player === 'O') {
+      $(event.target).html('O')
+    }
+  }
+
+  changePlayer()
   // when clicked, if the cell is blank, add a mark (x), if there is a mark already,
   // then leave the mark there.
-  $(event.target).html('X')
-  console.log('I got clicked!')
   // when clicked, if the cell is blank, add a mark (x), if there is a mark already,
   // then leave the mark there.
 
