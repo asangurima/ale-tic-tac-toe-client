ec3b4270af758f4301e51d9e8c17e3531a8e9346
asangurima <asangurima@gmail.com>
2019-02-17 16:39:53 -0500

    Add check for winner function
    -Add function that has the winning combinations and displays correct
      winner in events.js
    -Still need to solve problem of stopping game once winner is found
    -Need to print winner to user messages area

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,7 +1,32 @@
 
-let currentPlayer = 'X'
+let currentPlayer = 'x'
 // my javascript game board for gamelogic:
 let jsBoard = ['', '', '', '', '', '', '', '', '']
+// let cellIndexValue = ''
+
+// function to check for winner (after every move?)
+const checkForWinner = () => {
+  const rowOneWin = (((jsBoard[0] && jsBoard[1] && jsBoard[2]) === 'x') || ((jsBoard[0] && jsBoard[1] && jsBoard[2]) === 'o'))
+  const rowTwoWin = (((jsBoard[3] && jsBoard[4] && jsBoard[5]) === 'x') || ((jsBoard[3] && jsBoard[4] && jsBoard[5]) === 'o'))
+  const rowThreeWin = (((jsBoard[6] && jsBoard[7] && jsBoard[8]) === 'x') || ((jsBoard[6] && jsBoard[7] && jsBoard[8]) === 'o'))
+  const colOneWin = (((jsBoard[0] && jsBoard[3] && jsBoard[6]) === 'x') || ((jsBoard[0] && jsBoard[3] && jsBoard[6]) === 'o'))
+  const colTwoWin = (((jsBoard[1] && jsBoard[4] && jsBoard[7]) === 'x') || ((jsBoard[1] && jsBoard[4] && jsBoard[7]) === 'o'))
+  const colThreeWin = (((jsBoard[2] && jsBoard[5] && jsBoard[8]) === 'x') || ((jsBoard[2] && jsBoard[5] && jsBoard[8]) === 'o'))
+  const diagOneWin = (((jsBoard[0] && jsBoard[4] && jsBoard[8]) === 'x') || ((jsBoard[0] && jsBoard[4] && jsBoard[8]) === 'o'))
+  const diagTwoWin = (((jsBoard[2] && jsBoard[4] && jsBoard[6]) === 'x') || ((jsBoard[2] && jsBoard[4] && jsBoard[6]) === 'o'))
+
+  if ((rowOneWin === true) ||
+      (rowTwoWin === true) ||
+      (rowThreeWin === true) ||
+      (colOneWin === true) ||
+      (colTwoWin === true) ||
+      (colThreeWin === true) ||
+      (diagOneWin === true) ||
+      (diagTwoWin === true)) {
+    console.log(currentPlayer, 'is the winner!')
+  // need to stop the game in this function once a winner is declared!!
+  }
+}
 
 const onMakeMove = (event) => {
   event.preventDefault()
@@ -10,21 +35,39 @@ const onMakeMove = (event) => {
   const cellIndexValue = $(event.target).data('index')
   console.log('cellIndexValue:', cellIndexValue)
 
+  // const checkForWinner = () => {
+  //   console.log('checkForWinner is running!')
+  //   if (((jsBoard[0] === jsBoard[1]) === jsBoard[2]) ||
+  //     ((jsBoard[3] === jsBoard[4]) === jsBoard[5]) ||
+  //     ((jsBoard[6] === jsBoard[7]) === jsBoard[8]) ||
+  //     ((jsBoard[0] === jsBoard[4]) === jsBoard[8]) ||
+  //     ((jsBoard[2] === jsBoard[4]) === jsBoard[6]) ||
+  //     ((jsBoard[0] === jsBoard[3]) === jsBoard[6]) ||
+  //     ((jsBoard[1] === jsBoard[4]) === jsBoard[7]) ||
+  //     ((jsBoard[2] === jsBoard[5]) === jsBoard[8])) {
+  //     // need to stop game and tell users who won
+  //     console.log(currentPlayer, 'won the game!!!')
+  //   }
+  // }
+
   // make an x or o appear in cell when clicked; switch players:
   const changePlayer = () => {
-    if (currentPlayer === 'X') {
-      $(event.target).html('X')
+    if (currentPlayer === 'x') {
+      $(event.target).html('x')
       // push current player value to index in array correspondsing to the data
       // value of the cell (before switching players)
-      currentPlayer = 'O'
+      jsBoard.splice(cellIndexValue, 1, 'x')
+      checkForWinner()
+      currentPlayer = 'o'
       // tell the user who the current player is!
     } else {
-      $(event.target).html('O')
-      // push current player value to index in array correspondsing to the data
-      // value of the cell (before switching players)
-      currentPlayer = 'X'
+      $(event.target).html('o')
+      jsBoard.splice(cellIndexValue, 1, 'o')
+      checkForWinner()
+      currentPlayer = 'x'
       // tell the user who the current player is!
     }
+    console.log('jsBoard:', jsBoard)
   }
 
   if ($(event.target).html() === '') {
@@ -35,7 +78,7 @@ const onMakeMove = (event) => {
   }
 }
 
-// // reset button:
+// reset button:
 const onResetGame = (event) => {
   event.preventDefault()
   // clear cells
@@ -43,6 +86,21 @@ const onResetGame = (event) => {
   // clear JS gameboard
   jsBoard = ['', '', '', '', '', '', '', '', '']
 }
+
+// function to check for winner (after every move?)
+// const checkForWinner = () => {
+//   if (jsBoard[0] === jsBoard[1] === jsBoard[2] ||
+//     jsBoard[3] === jsBoard[4] === jsBoard[5] ||
+//     jsBoard[6] === jsBoard[7] === jsBoard[8] ||
+//     jsBoard[0] === jsBoard[4] === jsBoard[8] ||
+//     jsBoard[2] === jsBoard[4] === jsBoard[6] ||
+//     jsBoard[0] === jsBoard[3] === jsBoard[6] ||
+//     jsBoard[1] === jsBoard[4] === jsBoard[7] ||
+//     jsBoard[2] === jsBoard[5] === jsBoard[8]) {
+//     // need to stop game and tell users who won
+//     console.log(currentPlayer, 'won the game!!!')
+//   }
+// }
 
 module.exports = {
   onMakeMove,
