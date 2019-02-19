926fb25a8cd9540fa7a47df2600e3d5d1176a1ac
asangurima <asangurima@gmail.com>
2019-02-19 11:13:10 -0500

    Finish game logic and user messages
    -finish game logic function
    -add user message for turns
    -add user message for invalid move
    -add user message for win or draw
    -not allow users to add X or O after game is over
    -format user message text

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,80 +1,108 @@
 
 let currentPlayer = 'x'
+$('.userMessages').html('x starts the game!')
 // my javascript game board for gamelogic:
 let jsBoard = ['', '', '', '', '', '', '', '', '']
-// let cellIndexValue = ''
 
 // function to check for winner (after every move?)
 const checkForWinner = () => {
-  const rowOneWin = (((jsBoard[0] && jsBoard[1] && jsBoard[2]) === 'x') || ((jsBoard[0] && jsBoard[1] && jsBoard[2]) === 'o'))
-  const rowTwoWin = (((jsBoard[3] && jsBoard[4] && jsBoard[5]) === 'x') || ((jsBoard[3] && jsBoard[4] && jsBoard[5]) === 'o'))
-  const rowThreeWin = (((jsBoard[6] && jsBoard[7] && jsBoard[8]) === 'x') || ((jsBoard[6] && jsBoard[7] && jsBoard[8]) === 'o'))
-  const colOneWin = (((jsBoard[0] && jsBoard[3] && jsBoard[6]) === 'x') || ((jsBoard[0] && jsBoard[3] && jsBoard[6]) === 'o'))
-  const colTwoWin = (((jsBoard[1] && jsBoard[4] && jsBoard[7]) === 'x') || ((jsBoard[1] && jsBoard[4] && jsBoard[7]) === 'o'))
-  const colThreeWin = (((jsBoard[2] && jsBoard[5] && jsBoard[8]) === 'x') || ((jsBoard[2] && jsBoard[5] && jsBoard[8]) === 'o'))
-  const diagOneWin = (((jsBoard[0] && jsBoard[4] && jsBoard[8]) === 'x') || ((jsBoard[0] && jsBoard[4] && jsBoard[8]) === 'o'))
-  const diagTwoWin = (((jsBoard[2] && jsBoard[4] && jsBoard[6]) === 'x') || ((jsBoard[2] && jsBoard[4] && jsBoard[6]) === 'o'))
+  const rowOneWinX = (jsBoard[0] === 'x' && jsBoard[1] === 'x' && jsBoard[2] === 'x')
+  const rowOneWinO = (jsBoard[0] === 'o' && jsBoard[1] === 'o' && jsBoard[2] === 'o')
+  const rowTwoWinX = (jsBoard[3] === 'x' && jsBoard[4] === 'x' && jsBoard[5] === 'x')
+  const rowTwoWinO = (jsBoard[3] === 'o' && jsBoard[4] === 'o' && jsBoard[5] === 'o')
+  const rowThreeWinX = (jsBoard[6] === 'x' && jsBoard[7] === 'x' && jsBoard[8] === 'x')
+  const rowThreeWinO = (jsBoard[6] === 'o' && jsBoard[7] === 'o' && jsBoard[8] === 'o')
+  const colOneWinX = (jsBoard[0] === 'x' && jsBoard[3] === 'x' && jsBoard[6] === 'x')
+  const colOneWinO = (jsBoard[0] === 'o' && jsBoard[3] === 'o' && jsBoard[6] === 'o')
+  const colTwoWinX = (jsBoard[1] === 'x' && jsBoard[4] === 'x' && jsBoard[7] === 'x')
+  const colTwoWinO = (jsBoard[1] === 'o' && jsBoard[4] === 'o' && jsBoard[7] === 'o')
+  const colThreeWinX = (jsBoard[2] === 'x' && jsBoard[5] === 'x' && jsBoard[8] === 'x')
+  const colThreeWinO = (jsBoard[2] === 'o' && jsBoard[5] === 'o' && jsBoard[8] === 'o')
+  const diagOneWinX = (jsBoard[0] === 'x' && jsBoard[4] === 'x' && jsBoard[8] === 'x')
+  const diagOneWinO = (jsBoard[0] === 'o' && jsBoard[4] === 'o' && jsBoard[8] === 'o')
+  const diagTwoWinX = (jsBoard[2] === 'x' && jsBoard[4] === 'x' && jsBoard[6] === 'x')
+  const diagTwoWinO = (jsBoard[2] === 'o' && jsBoard[4] === 'o' && jsBoard[6] === 'o')
 
-  if ((rowOneWin === true) ||
-      (rowTwoWin === true) ||
-      (rowThreeWin === true) ||
-      (colOneWin === true) ||
-      (colTwoWin === true) ||
-      (colThreeWin === true) ||
-      (diagOneWin === true) ||
-      (diagTwoWin === true)) {
-    console.log(currentPlayer, 'is the winner!')
+  if ((rowOneWinX === true) ||
+      (rowOneWinO === true) ||
+      (rowTwoWinX === true) ||
+      (rowTwoWinO === true) ||
+      (rowThreeWinX === true) ||
+      (rowThreeWinO === true) ||
+      (colOneWinX === true) ||
+      (colOneWinO === true) ||
+      (colTwoWinX === true) ||
+      (colTwoWinO === true) ||
+      (colThreeWinX === true) ||
+      (colThreeWinO === true) ||
+      (diagOneWinX === true) ||
+      (diagOneWinO === true) ||
+      (diagTwoWinX === true) ||
+      (diagTwoWinO === true)) {
+    // console.log(currentPlayer, 'is the winner!')
+    $('.userMessages').html(currentPlayer + ' is the winner!')
+    gameOver()
   // need to stop the game in this function once a winner is declared!!
+  } else if (notEmpty(jsBoard)) {
+    $('.userMessages').html(`It's a draw!`)
   }
+}
+
+const gameOver = () => {
+  $('.cell').off('click', onMakeMove)
+}
+
+const notEmpty = (arr) => {
+  const isNotEmptyString = (value) => {
+    return value !== ''
+  }
+  return arr.every(isNotEmptyString)
 }
 
 const onMakeMove = (event) => {
   event.preventDefault()
-  //  if I grab the data index-number now, I can then put an X in that specific,
-  // data index # cell?
+
+  const isValidMove = () => {
+    if ($(event.target).html() !== '') {
+      $('.userMessages').html(`Not a valid move.`)
+
+      setTimeout(() => {
+        $('.userMessages').html(`It's ${currentPlayer}'s turn!`)
+      }, 1000)
+    }
+  }
+
+  isValidMove()
+
   const cellIndexValue = $(event.target).data('index')
   console.log('cellIndexValue:', cellIndexValue)
-
-  // const checkForWinner = () => {
-  //   console.log('checkForWinner is running!')
-  //   if (((jsBoard[0] === jsBoard[1]) === jsBoard[2]) ||
-  //     ((jsBoard[3] === jsBoard[4]) === jsBoard[5]) ||
-  //     ((jsBoard[6] === jsBoard[7]) === jsBoard[8]) ||
-  //     ((jsBoard[0] === jsBoard[4]) === jsBoard[8]) ||
-  //     ((jsBoard[2] === jsBoard[4]) === jsBoard[6]) ||
-  //     ((jsBoard[0] === jsBoard[3]) === jsBoard[6]) ||
-  //     ((jsBoard[1] === jsBoard[4]) === jsBoard[7]) ||
-  //     ((jsBoard[2] === jsBoard[5]) === jsBoard[8])) {
-  //     // need to stop game and tell users who won
-  //     console.log(currentPlayer, 'won the game!!!')
-  //   }
-  // }
 
   // make an x or o appear in cell when clicked; switch players:
   const changePlayer = () => {
     if (currentPlayer === 'x') {
       $(event.target).html('x')
       // push current player value to index in array correspondsing to the data
-      // value of the cell (before switching players)
+      // value of the cell (before switching players):
       jsBoard.splice(cellIndexValue, 1, 'x')
+      $('.userMessages').html(`It's o's turn!`)
       checkForWinner()
       currentPlayer = 'o'
       // tell the user who the current player is!
     } else {
       $(event.target).html('o')
       jsBoard.splice(cellIndexValue, 1, 'o')
+      $('.userMessages').html(`It's x's turn!`)
       checkForWinner()
       currentPlayer = 'x'
       // tell the user who the current player is!
     }
     console.log('jsBoard:', jsBoard)
+    // console.log('all x or o', allXOrO(jsBoard))
   }
 
   if ($(event.target).html() === '') {
     changePlayer()
     // need to figure out a way to store the currentPlayer in store file?
-    // want to push the cellIndexValue to the jsBoard array
-    // it will correspond to the index # in the JS game board array
   }
 }
 
@@ -83,24 +111,11 @@ const onResetGame = (event) => {
   event.preventDefault()
   // clear cells
   $('.cell').html('')
+  $('.userMessages').html('x starts the game!')
   // clear JS gameboard
   jsBoard = ['', '', '', '', '', '', '', '', '']
+  $('.cell').on('click', onMakeMove)
 }
-
-// function to check for winner (after every move?)
-// const checkForWinner = () => {
-//   if (jsBoard[0] === jsBoard[1] === jsBoard[2] ||
-//     jsBoard[3] === jsBoard[4] === jsBoard[5] ||
-//     jsBoard[6] === jsBoard[7] === jsBoard[8] ||
-//     jsBoard[0] === jsBoard[4] === jsBoard[8] ||
-//     jsBoard[2] === jsBoard[4] === jsBoard[6] ||
-//     jsBoard[0] === jsBoard[3] === jsBoard[6] ||
-//     jsBoard[1] === jsBoard[4] === jsBoard[7] ||
-//     jsBoard[2] === jsBoard[5] === jsBoard[8]) {
-//     // need to stop game and tell users who won
-//     console.log(currentPlayer, 'won the game!!!')
-//   }
-// }
 
 module.exports = {
   onMakeMove,
